b8bddef78d5f943a160781dead8e2ecb0dbe3d4c
Conde <picopixelweb@gmail.com>
2019-06-01 22:16:06 -0300

    boton_10000

--- index.php
+++ index.php
@@ -144,7 +144,7 @@
 
 									
 							<p class="card-text"><small class="text-muted">A un precio final de $10000</small></p>
-							<a mp-mode="dftl" href="https://www.mercadopago.com/mla/checkout/start?pref_id=39635882-1dcd4ec7-23a3-49b6-b947-ca7026dd8b17" name="MP-payButton" class='blue-ar-l-rn-none'>Pagar</a>
+							<a mp-mode="dftl" href="https://www.mercadopago.com/mla/checkout/start?pref_id=39635882-1dcd4ec7-23a3-49b6-b947-ca7026dd8b17" name="MP-payButton" class='green-ar-l-rn-arall'>Pagar</a>
 <script type="text/javascript">
 (function(){function $MPC_load(){window.$MPC_loaded !== true && (function(){var s = document.createElement("script");s.type = "text/javascript";s.async = true;s.src = document.location.protocol+"//secure.mlstatic.com/mptools/render.js";var x = document.getElementsByTagName('script')[0];x.parentNode.insertBefore(s, x);window.$MPC_loaded = true;})();}window.$MPC_loaded !== true ? (window.attachEvent ?window.attachEvent('onload', $MPC_load) : window.addEventListener('load', $MPC_load, false)) : null;})();
 </script>
