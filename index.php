c2168653d28cadd9f2fc90bd7a4da3e218d721fa
Conde <picopixelweb@gmail.com>
2019-06-07 00:33:44 -0300

    btn_pago1

--- index.php
+++ index.php
@@ -50,9 +50,9 @@
 														LINK A REDES SOCIALES</p>
 									<p class="card-text"><small class="text-muted">A un precio final de $2000<br><br><br><br><br><br><br><br><br></small></p>
 									<a mp-mode="dftl" href="https://www.mercadopago.com/mla/checkout/start?pref_id=39635882-fb3b876e-8e38-497a-87f0-1da5ca30713c" name="MP-payButton" class='green-ar-l-rn-arall'>Pagar</a>
-<script type="text/javascript">
-(function(){function $MPC_load(){window.$MPC_loaded !== true && (function(){var s = document.createElement("script");s.type = "text/javascript";s.async = true;s.src = document.location.protocol+"//secure.mlstatic.com/mptools/render.js";var x = document.getElementsByTagName('script')[0];x.parentNode.insertBefore(s, x);window.$MPC_loaded = true;})();}window.$MPC_loaded !== true ? (window.attachEvent ?window.attachEvent('onload', $MPC_load) : window.addEventListener('load', $MPC_load, false)) : null;})();
-</script>
+									<script type="text/javascript">
+									(function(){function $MPC_load(){window.$MPC_loaded !== true && (function(){var s = document.createElement("script");s.type = "text/javascript";s.async = true;s.src = document.location.protocol+"//secure.mlstatic.com/mptools/render.js";var x = document.getElementsByTagName('script')[0];x.parentNode.insertBefore(s, x);window.$MPC_loaded = true;})();}window.$MPC_loaded !== true ? (window.attachEvent ?window.attachEvent('onload', $MPC_load) : window.addEventListener('load', $MPC_load, false)) : null;})();
+									</script>
 
 									
 									
